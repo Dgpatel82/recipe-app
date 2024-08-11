dcd6df20c11cb4274e4540ab35db3202a32cfb3b
Dgpatel82 <dgpatel828@gmail.com>
2024-08-11 10:33:58 +0530

    Updates

---
+++ precache-manifest.5c1583479f74498f34698034e4d07b3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2037fc4649e340207e80aa6ccb67183c",
+    "url": "/Dgpatel82/recipe-app/index.html"
+  },
+  {
+    "revision": "5f94cd8c11fd62d24d41",
+    "url": "/Dgpatel82/recipe-app/static/css/main.129f6fb5.chunk.css"
+  },
+  {
+    "revision": "e953909b1714aea639da",
+    "url": "/Dgpatel82/recipe-app/static/js/2.9069b3e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Dgpatel82/recipe-app/static/js/2.9069b3e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f94cd8c11fd62d24d41",
+    "url": "/Dgpatel82/recipe-app/static/js/main.07c60ed8.chunk.js"
+  },
+  {
+    "revision": "df41adc308d6875c6df2",
+    "url": "/Dgpatel82/recipe-app/static/js/runtime-main.e4a16bb8.js"
+  }
+]);
